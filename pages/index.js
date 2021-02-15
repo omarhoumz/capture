219167d424346c012dc22aa5065cad23bfc97d09
Omar Houmz <omar.houmz@adevinta.com>
2021-02-15 18:59:38 +0100

    chore: add qr generator

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,47 @@
+import React from "react";
 import Head from "next/head";
+import QrScanner from "qr-scanner";
 import styles from "../styles/Home.module.css";
+import QRCode from "react-qr-code";
+
+const constraints = {
+  video: { width: 320, height: 320 },
+};
 
 export default function Home() {
-  // const enumeratorPromise =
-  //   typeof window !== "undefined" && navigator.mediaDevices.enumerateDevices();
+  const [permissionGranted, setPermissionGranted] = React.useState(false);
+  const [haveCode, setHaveCode] = React.useState(null);
 
-  // enumeratorPromise.then((res) => {
-  //   console.log(res);
-  // });
+  React.useEffect(() => {
+    if (permissionGranted) {
+      const player = document.getElementById("player");
+      // const canvas = document.getElementById("canvas");
+      // const context = canvas.getContext("2d");
+      // const captureButton = document.getElementById("capture");
 
-  if (typeof window !== "undefined") {
-    const player = document.getElementById("player");
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
-    const captureButton = document.getElementById("capture");
+      // captureButton.addEventListener("click", () => {
+      //   // Draw the video frame to the canvas.
+      //   context.drawImage(player, 0, 0, canvas.width, canvas.height);
+      // });
 
-    const constraints = {
-      video: true,
-    };
+      const qrScanner = new QrScanner(player, (result) => setHaveCode(result));
 
-    captureButton.addEventListener("click", () => {
-      // Draw the video frame to the canvas.
-      context.drawImage(player, 0, 0, canvas.width, canvas.height);
-    });
+      qrScanner.start();
 
-    // Attach the video stream to the video element and autoplay.
-    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-      player.srcObject = stream;
-      player.onloadedmetadata = function (e) {
-        player.play();
-      };
-    });
+      // Attach the video stream to the video element and autoplay.
+      navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        player.srcObject = stream;
+        player.onloadedmetadata = function (e) {
+          player.play();
+        };
+      });
+    }
+  }, [permissionGranted]);
+
+  function handleScanRequest(e) {
+    setPermissionGranted(
+      Boolean(navigator.mediaDevices.getUserMedia(constraints))
+    );
   }
 
   return (
@@ -41,10 +52,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <video id='player' />
-        <button id='capture'>Capture</button>
-        <div style={{ flexGrow: 1 }}>
-          <canvas id='canvas' width='320' height='240'></canvas>
+        {permissionGranted && !haveCode ? (
+          <div
+            style={{
+              minWidth: constraints.video.width,
+              minHeight: constraints.video.height,
+            }}
+            className={styles.videoFeed}
+          >
+            <video id='player' />
+          </div>
+        ) : (
+          <div>
+            <div>{haveCode}</div>
+            <button onClick={handleScanRequest} type='submit'>
+              Scann Code
+            </button>
+          </div>
+        )}
+        <div>
+          <QRCode value='hey' />
         </div>
       </main>
     </div>
